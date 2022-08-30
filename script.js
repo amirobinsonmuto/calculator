87e48f96f424d6c6f59ece6b9dfef65a8760df46
AmiRobinsonMuto <amimuto1002@gmail.com>
2022-08-30 11:18:03 -0700

    Final edits

--- script.js
+++ script.js
@@ -1,102 +1,119 @@
 //declare global variables
-let num1 = null;
-let num2 = null;
-let input = ""
-let operatorId =""
-const display = document.getElementById("display");
+    let num1 = null;
+    let num2 = null;
+    let input = ""
+    let operatorId =""
+    const display = document.getElementById("display");
+
+//global functions to be called in to the main functions
+    //calculation function
+    function calculation(a, b) {
+
+        //perform calculation
+        if (operatorId === "division") {
+            num1 = a / b;
+        } else if (operatorId === "multiplication") {
+            num1 = a * b;
+        } else if (operatorId === "addition") {
+            num1 = a + b;
+        } else {
+            num1 = a - b;
+        }
+    }
+
 
 //add event listener to number buttons and store the value in input
-const numberButtons = document.querySelectorAll(".numberButton");
+    const numberButtons = document.querySelectorAll(".numberButton");
 
-numberButtons.forEach((numberButton) => {
-    numberButton.addEventListener('click', ()=>{
-        input += numberButton.value;
-        display.textContent = input;
+    numberButtons.forEach((numberButton) => {
+        numberButton.addEventListener('click', ()=>{
+            if (input.length < 16) {
+                input += numberButton.value;
+                display.textContent = input;
+            }
+        })
     })
-})
+
+//add event listener to a point button and store the value in input
+    const pointButton = document.querySelector(".point");
+
+    pointButton.addEventListener('click', ()=>{
+            
+            //if the current input contains a point, do not allow another point to be added it input
+            if (!input.includes(".")) {
+                input += pointButton.value;
+                display.textContent = input;
+            } 
+        })
 
 //operator functions
-const operators = document.querySelectorAll(".operator");
+    const operators = document.querySelectorAll(".operator");
 
-operators.forEach((operator) => {
-    operator.addEventListener('click', ()=>{
+    operators.forEach((operator) => {
+        operator.addEventListener('click', ()=>{
 
-        //if num1 is empty, convert input string to an integer and store it in num 1
-        //if num1 has a value, convert input string to and integer and store it in num2 and execute calculation
-        if(num1 === null){
-            num1 = parseFloat(input); 
-        } else {                      
-            num2 = parseFloat(input);
-            calculation(num1, num2);
-        }
+            //if num1 is empty, convert input string to an integer and store it in num 1
+            //if num1 has a value, convert input string to and integer and store it in num2 and execute calculation
+            if(num1 === null){
+                num1 = parseFloat(input); 
+            } else {                      
+                num2 = parseFloat(input);
+                calculation(num1, num2);
+            }
 
-        //store the operation id in the operatorId variable for the next calculation
-        operatorId = operator.id; 
+            //store the operation id in the operatorId variable for the next calculation
+            operatorId = operator.id; 
 
-        //clear input
-        input = ""
+            //clear input
+            input = ""
+
+            //display
+            display.textContent = num1; 
+
+            console.log(`num1 ${num1}`);
+            console.log(`input ${input}`)
+            console.log(`num2 ${num2}`);
+            console.log(`operatorId ${operatorId}`);
+        })
+    })
+
+
+// equal function
+    const equal = document.getElementById("equal");
+
+    equal.addEventListener('click', ()=>{
+
+        //store input in num2. It's always num2
+        num2 = parseInt(input);
+
+        //perform calculation
+        calculation(num1, num2);
+
+        //store num1 in input so that a subsequent calculation will have an input value
+        input = num1;
 
         //display
-        display.textContent = num1; 
+        display.textContent = num1;
+
+        //reset num1 and num2;
+        num1 = null;
+        num2 = null;
 
         console.log(`num1 ${num1}`);
         console.log(`input ${input}`)
         console.log(`num2 ${num2}`);
         console.log(`operatorId ${operatorId}`);
     })
- })
-
-
-// equal function
-const equal = document.getElementById("equal");
-
-equal.addEventListener('click', ()=>{
-
-    //store input in num2. It's always num2
-    num2 = parseInt(input);
-
-    //perform calculation
-    calculation(num1, num2);
-
-    //store num1 in input so that a subsequent calculation will have an input value
-    input = num1;
-
-    //display
-    display.textContent = num1;
-
-    //reset num1 and num2;
-    num1 = null;
-    num2 = null;
-
-    console.log(`num1 ${num1}`);
-    console.log(`input ${input}`)
-    console.log(`num2 ${num2}`);
-    console.log(`operatorId ${operatorId}`);
-})
 
 //clear function
-const clear = document.getElementById("clear");
+    const clear = document.getElementById("clear");
 
-clear.addEventListener('click', ()=>{
-    num1 = null;
-    num2 = null;
-    input = "";
-    display.textContent = ""
-})
+    clear.addEventListener('click', ()=>{
+        num1 = null;
+        num2 = null;
+        input = "";
+        display.textContent = ""
+    })
 
-//calculation function
-function calculation(a, b) {
-
-    //perform calculation
-    if (operatorId === "division") {
-        num1 = a / b;
-    } else if (operatorId === "multiplication") {
-        num1 = a * b;
-    } else if (operatorId === "addition") {
-        num1 = a + b;
-    } else {
-        num1 = a - b;
-    }
-}
 
 
